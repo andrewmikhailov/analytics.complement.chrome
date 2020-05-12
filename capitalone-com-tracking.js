4548920a830271900768a471e27f4fadd145283a
Vitche Research Team <developer@vitche.com>
2020-05-12 09:16:10 -0400

    1. Added code injection for CapitalOne.

---
+++ capitalone-com-tracking.js
@@ -0,0 +1,7 @@
+console.log('Injected capitalone-com-tracking.js');
+(function () {
+    let i = 0;
+    const interval = setInterval(() => {
+        console.log('Timers demo:', i++);
+    }, 3000);
+})();
